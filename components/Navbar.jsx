992953cb1f0496bcfb7261593551a01aab2816ee
Kirthigesan <kirthi101996@gmail.com>
2024-07-09 12:34:05 +0200

    add

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,41 +1,41 @@
-import { useContext } from "react"
-import { AuthContext } from "../context/AuthContextWrapper"
-import { NavLink } from "react-router-dom"
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContextWrapper";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
-    const { user, isLoggedIn, disconnect } = useContext(AuthContext)
-
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <NavLink to="/">HomePage</NavLink>
-                </li>
-                {!isLoggedIn ? (
-                    <>
-                        <li>
-                            <NavLink to={"/signup"}>Sign up</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={"/login"}>Login</NavLink>
-                        </li>
-                    </>
-                ) : (
-                    <>
-                        <li>
-                            <button onClick={disconnect}>Logout</button>
-                        </li>
-                        <li>
-                            <p>Welcome back {user.username}</p>
-                        </li>
-                        <li>
-                            <NavLink to={"/create"}>Create activity</NavLink>
-                        </li>
-                    </>
-                )}
-            </ul>
-        </nav>
-    )
+  const { user, isLoggedIn, disconnect } = useContext(AuthContext);
+  console.log(user);
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">HomePage</NavLink>
+        </li>
+        {!isLoggedIn ? (
+          <>
+            <li>
+              <NavLink to={"/signup"}>Sign up</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/login"}>Login</NavLink>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <button onClick={disconnect}>Logout</button>
+            </li>
+            <li>
+              <p>Welcome back {user}</p>
+            </li>
+            <li>
+              <NavLink to={"/create"}>Create activity</NavLink>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
