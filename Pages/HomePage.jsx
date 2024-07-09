87b4a7933f698c36a1810c5c64fa1cb76d8a1407
Kirthigesan <kirthi101996@gmail.com>
2024-07-09 16:07:01 +0200

    add

--- Pages/HomePage.jsx
+++ Pages/HomePage.jsx
@@ -1,14 +1,28 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router";
 import service from "../service/api";
 import { AuthContext } from "../context/AuthContextWrapper";
-function HomePage() {
-  const { user } = useContext(AuthContext);
-  console.log(user);
-  const data = user.user;
 
-  async function deleteUser(userId) {
-    await service.delete(`/${userId}`);
+function HomePage() {
+  const { user, disconnect, removeToken, removeUserId } =
+    useContext(AuthContext);
+  const data = user.user;
+  const navigate = useNavigate();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await service.delete(`/user/${user.user._id}`);
+      console.log(response);
+
+      if (response) {
+        removeToken();
+        removeUserId();
+        navigate("/login");
+      }
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
@@ -16,7 +30,7 @@ function HomePage() {
       <h1>{data.name}</h1>
       <img
         src={data.image}
-        alt=""
+        alt="User Profile"
         style={{ borderRadius: "50%", width: 100 }}
       />
       <h4>{data.age}</h4>
@@ -25,11 +39,12 @@ function HomePage() {
       <h4>{data.gender}</h4>
       <h4>Total calories Burned: {user.totalCaloriesBurnt}</h4>
       <h4>Total workouts: {user.totalworkouts}</h4>
-      {/* {user.workoutsList.length >= 1 ? (
-        <h4>{user.workoutsList}</h4>
-      ) : (
-        <h4>No workouts</h4>
-      )} */}
+      <button
+        className="bg-red-500 text-white p-2 rounded"
+        onClick={handleSubmit}
+      >
+        Delete User
+      </button>
     </div>
   );
 }
