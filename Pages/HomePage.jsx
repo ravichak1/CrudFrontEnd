59c45c2cde86361980e4de299b1d3ab4252ea316
Kirthigesan <kirthi101996@gmail.com>
2024-07-09 16:09:01 +0200

    add

--- Pages/HomePage.jsx
+++ Pages/HomePage.jsx
@@ -1,27 +1,27 @@
-import React, { useContext } from "react";
-import { useNavigate } from "react-router";
+import React from "react";
+import { useContext } from "react";
 import service from "../service/api";
 import { AuthContext } from "../context/AuthContextWrapper";
-
+import axios from "axios";
+import { useNavigate } from "react-router";
 function HomePage() {
-  const { user, disconnect, removeToken, removeUserId } =
-    useContext(AuthContext);
+  const { user, removeToken, removeUserId } = useContext(AuthContext);
   const data = user.user;
+  console.log(user.user._id);
   const navigate = useNavigate();
-
   async function handleSubmit(event) {
     event.preventDefault();
     try {
       const response = await service.delete(`/user/${user.user._id}`);
       console.log(response);
 
-      if (response) {
-        removeToken();
-        removeUserId();
+      if (response.status === 200) {
+        removeToken;
+        removeUserId;
         navigate("/login");
       }
     } catch (error) {
-      console.error(error);
+      console.log(error);
     }
   }
 
@@ -30,7 +30,7 @@ function HomePage() {
       <h1>{data.name}</h1>
       <img
         src={data.image}
-        alt="User Profile"
+        alt=""
         style={{ borderRadius: "50%", width: 100 }}
       />
       <h4>{data.age}</h4>
@@ -39,9 +39,17 @@ function HomePage() {
       <h4>{data.gender}</h4>
       <h4>Total calories Burned: {user.totalCaloriesBurnt}</h4>
       <h4>Total workouts: {user.totalworkouts}</h4>
+      {/* {user.workoutsList.length >= 1 ? (
+        <h4>{user.workoutsList}</h4>
+      ) : (
+        <h4>No workouts</h4>
+      )} */}
       <button
-        className="bg-red-500 text-white p-2 rounded"
-        onClick={handleSubmit}
+        className="bg-red-500"
+        onClick={() => {
+          console.log(`hello ${user.user._id}`);
+          handleSubmit(user.user._id);
+        }}
       >
         Delete User
       </button>
